ee58a4ed174ca1a36da6ace749b1949bc26df0c2
Pierre Segalen <psegalen@outlook.com>
2023-12-14 10:20:51 +0100

    One ESLint ignore was missing

--- src/TreeView/index.tsx
+++ src/TreeView/index.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const cx = require("classnames");
 import PropTypes from "prop-types";
 import React, { useEffect, useReducer, useRef } from "react";
